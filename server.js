a27a9f22f2f76c3efd9e9f6095061ef3c76e4a6b
lukascobit <lukyn.odehnal333@gmail.com>
2022-12-23 22:29:09 +0100

    test

--- server.js
+++ server.js
@@ -1,62 +1,64 @@
-const express = require('express');
-const mysql = require('mysql')
-require('dotenv').config();
-const app = express()
-const cors = require("cors")
+const express = require("express");
+const mysql = require("mysql");
+require("dotenv").config();
+const app = express();
+const cors = require("cors");
 
 const db = mysql.createPool({
-    host: "bgvqojweb2qinrqzgchr-mysql.services.clever-cloud.com",
-    user: "u7p2yoidmtbhyp1i",
-    password: process.env.PASSWORD,
-    database: "bgvqojweb2qinrqzgchr",
-    charset : 'utf8mb4'
-})
-
-// db.connect()
+	host: "bgvqojweb2qinrqzgchr-mysql.services.clever-cloud.com",
+	user: "u7p2yoidmtbhyp1i",
+	password: process.env.PASSWORD,
+	database: "bgvqojweb2qinrqzgchr",
+	charset: "utf8mb4",
+});
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
-// TODO: HOST also the server 
-
-
 //get all drby
-app.get("/drby", (req, res)=>{
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    const sql = `
+app.get("/drby", (req, res) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept"
+	);
+	const sql = `
     SELECT 
     *,
     DATE_FORMAT(datum, '%d/%m/%Y %H:%i') AS posted_date
-    FROM drby;`
-    db.query(sql, (err, result)=>{
-        if(err) throw err;
-        res.send(result);
-    })
+    FROM drby;`;
+	db.query(sql, (err, result) => {
+		if (err) throw err;
+		console.log(result);
+		res.send(result);
+	});
 });
 
-
 //add a blog post
-app.post("/drby", (req, res)=>{
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+app.post("/drby", (req, res) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept"
+	);
 
-    const post = req.body
-    const sql = `
+	const post = req.body;
+	const sql = `
     INSERT INTO drby(username, body, datum, genre) VALUES(?, ?, now(), ?) ;
-    `
-    console.log(post);
-    db.query(sql,[post[1].username, post[0].body, post[2].genre], (err, result)=>{
-        if(err) throw err;
-        res.send(result)
-    })
-})
+    `;
+	console.log(post);
+	db.query(
+		sql,
+		[post[1].username, post[0].body, post[2].genre],
+		(err, result) => {
+			if (err) throw err;
+			res.send(result);
+		}
+	);
+});
 
-
-
-const port = process.env.PORT
-app.listen(port , ()=>{
-    console.log(`listening on port ${port}`);
-    
-})
+const port = process.env.PORT;
+app.listen(port, () => {
+	console.log(`listening on port ${port}`);
+});
